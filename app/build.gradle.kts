d5ab0f6c9ecc92475f63a69745204393a9e89b41
Djerfi Fatma <154927228+fatmakhadidja@users.noreply.github.com>
2026-04-15 18:17:13 +0100

    fixed scroll issue and finished passport scan page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,8 @@ dependencies {
     implementation(libs.androidx.camera.camera2)
     implementation(libs.androidx.camera.lifecycle)
     implementation(libs.androidx.camera.view)
-    
+    implementation(libs.androidx.compose.foundation)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
