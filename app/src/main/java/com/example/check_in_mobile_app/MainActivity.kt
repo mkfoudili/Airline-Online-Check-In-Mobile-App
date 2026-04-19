35d81ebe4f767c30a83ec38661624166539ada5c
Khadidja <mk_foudili@esi.dz>
2026-04-20 00:42:57 +0100

    Switch to AppCompatActivity and add locale configuration

--- app/src/main/java/com/example/check_in_mobile_app/MainActivity.kt
+++ app/src/main/java/com/example/check_in_mobile_app/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.check_in_mobile_app
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.check_in_mobile_app.presentation.navigation.AppNavGraph
 import com.example.check_in_mobile_app.ui.theme.CheckInMobileAppTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
