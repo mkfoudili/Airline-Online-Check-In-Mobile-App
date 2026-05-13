5647f7e5727e9918e22b89ebf8ea0970f60542b7
Khadidja <mk_foudili@esi.dz>
2026-05-13 19:28:06 +0100

    Add ProfileDto, ProfileResponse, and ProfileMapper

---
+++ data/src/main/java/com/example/data/remote/dto/ProfileDto.kt
@@ -0,0 +1,16 @@
+package com.example.data.remote.dto
+
+data class ProfileResponse(
+    val success: Boolean,
+    val data: ProfileDto?,
+    val message: String?
+)
+
+data class ProfileDto(
+    val uid: String,
+    val email: String,
+    val fullName: String,
+    val phoneNumber: String?,
+    val avatarUrl: String?,
+    val isVerified: Boolean
+)
