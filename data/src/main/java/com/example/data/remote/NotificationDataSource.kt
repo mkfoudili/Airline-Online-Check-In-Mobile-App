719a025aec2c826a592a290c87c3f24a6cd594b1
Khadidja <mk_foudili@esi.dz>
2026-05-28 20:10:32 +0100

    Add profile, password, and baggage endpoints to Retrofit interface

--- data/src/main/java/com/example/data/remote/NotificationDataSource.kt
+++ data/src/main/java/com/example/data/remote/NotificationDataSource.kt
@@ -1,6 +1,5 @@
 package com.example.data.remote
 
-import com.example.data.remote.dto.NotificationDto
 import com.example.data.remote.dto.NotificationListResponse
 import com.example.data.remote.dto.NotificationResponse
 import com.example.data.remote.dto.ReadAllResponse
@@ -16,39 +15,8 @@ class NotificationDataSource @Inject constructor(
         return endpoint.getNotifications()
     }
 
-    fun getNotifications(uid: String, callback: (Result<List<NotificationDto>>) -> Unit) {
-        MySqlHelper.getConnection { connection ->
-            if (connection != null) {
-                try {
-                    val sql = "SELECT * FROM NOTIFICATIONS WHERE uid = ? ORDER BY createdAt DESC"
-                    val preparedStatement = connection.prepareStatement(sql)
-                    preparedStatement.setString(1, uid)
-
-                    val resultSet: ResultSet = preparedStatement.executeQuery()
-                    val notifications = mutableListOf<NotificationDto>()
-                    while (resultSet.next()) {
-                        val notification = NotificationDto(
-                            notificationId = resultSet.getString("notificationId"),
-                            uid = resultSet.getString("uid"),
-                            passengerId = resultSet.getString("passengerId"),
-                            flightId = resultSet.getString("flightId"),
-                            type = resultSet.getString("type"),
-                            title = resultSet.getString("title"),
-                            body = resultSet.getString("body"),
-                            isRead = resultSet.getBoolean("isRead"),
-                            createdAt = resultSet.getTimestamp("createdAt")
-                        )
-                        notifications.add(notification)
-                    }
-                    callback(Result.success(notifications))
-                    connection.close()
-                } catch (e: SQLException) {
-                    callback(Result.failure(e))
-                }
-            } else {
-                callback(Result.failure(Exception("Could not connect to database")))
-            }
-        }
+    suspend fun markAsRead(notificationId: String): NotificationResponse {
+        return endpoint.markAsRead(notificationId)
     }
 
     suspend fun markAllAsRead(): ReadAllResponse {
