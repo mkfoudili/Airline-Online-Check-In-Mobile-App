710d3c186899156c668e98f69fe58a54b32e495c
Melliti Abdelmalek <ma_melliti@esi.dz>
2026-05-31 11:47:02 +0100

    Fixed a small bug in boarding pass qr code

--- data/src/main/java/com/example/data/local/dao/FlightDao.kt
+++ data/src/main/java/com/example/data/local/dao/FlightDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Upsert
 import com.example.data.local.entity.FlightEntity
 
 @Dao
@@ -16,6 +17,9 @@ interface FlightDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertFlight(entity: FlightEntity)
+
+    @Upsert
+    suspend fun insertFlights(entities: List<FlightEntity>)
 
     @Query("DELETE FROM flights")
     suspend fun deleteAllFlights()
